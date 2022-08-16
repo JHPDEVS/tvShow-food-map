207489718c2b809a0d7444992c3f2d3ba676ab53
JHPDEVS <jhpdevs@g.yju.ac.kr>
2022-08-16 15:20:27 +0900

    main page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import logo from './logo.svg'
-import './App.css'
+import './dist/output.css'
 import bg from './img/bg.gif'
-const backgroundArr = [bg]
+import bg2 from './img/bg2.gif'
+import bg3 from './img/bg3.gif'
+import logo from './img/food_map.png'
+import Button from './Button.js'
+import { Link } from 'react-router-dom'
+const backgroundArr = [bg, bg2, bg3]
 const randomIndex = Math.floor(Math.random() * backgroundArr.length)
 const backgroundImg = backgroundArr[randomIndex]
 function App() {
   return (
     <div
       style={{
-        backgroundImage: `url(${bg})`,
+        backgroundImage: `url(${backgroundImg})`,
       }}
-      className="w-full h-full bg-food  mx-auto bg-gray-200 rounded-md flex items-center bg-blend-multiply bg-no-repeat bg-cover"
+      className="w-full h-full bg-food  mx-auto bg-gray-400  flex items-center bg-blend-multiply bg-no-repeat bg-cover"
     >
       <div class="hero min-h-screen ">
         <div class="hero-content text-center">
           <div class="max-w-md">
-            <h1 class="text-5xl font-bold text-white">안녕하세요!</h1>
-            <h2>방송에서 나온 맛집을 찾아보세요!</h2>
-            <input
-              type="text"
-              placeholder="Type here"
-              class="input input-bordered input-primary w-full max-w-xs m-6"
-            />
-            <button class="btn btn-primary">Get Started</button>
+            <div className=" animate-[bounce_6s_ease-in-out_infinite]">
+              <img src={logo} />
+              <h2 className="text-white font-bold text-2xl">
+                방송에서 나온 맛집을 찾아보세요!
+              </h2>
+            </div>
+
+            <div class="flex justify-center mt-3">
+              <button class="text-white text-2xl font-bold"></button>
+              <div class="alert shadow-lg items-center justify-center">
+                <button class="alert btn ">
+                  <Link to="/main">접속하기</Link>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './dist/output.css'
+import AnimatedCursor from 'react-animated-cursor'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
+    <AnimatedCursor
+      innerSize={20}
+      outerSize={20}
+      color="1, 200, 111"
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+    />
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<App />} />
-        <Route path="teams" element={<App />}></Route>
+        <Route path="/main" element={<App />}></Route>
       </Route>
     </Routes>
   </BrowserRouter>
